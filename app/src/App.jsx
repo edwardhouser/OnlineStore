50cb2e65c8b6346d760dd59c3d5f3dcb9e0f6d7e
edwardhouser <edward.houser@gmail.com>
2025-03-26 14:40:11 -0700

    26 MAR Commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -11,19 +11,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/Cart'
 
 
 function App() {
 
 
   return (
-    <>
+    <BrowserRouter> {/* this is called a wrapper */}
       <Navbar></Navbar>
-      <Catalog></Catalog>
-      <About></About>
-      <Admin></Admin>
+      <Routes> {/* routes shows only the selected component, and hides everything else */}
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/catalog' element={<Catalog></Catalog>} />
+        <Route path='/about' element={<About></About>} />
+        <Route path='/admin' element={<Admin></Admin>} />
+        <Route path='/cart' element={<Cart />} />
+        
+      </Routes>
+      
+      
+      
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   )
 }
 
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import "./Navbar.css";
 
+import { Link } from "react-router-dom";
+
 function Navbar(){
     return (
         <div className="navbar">
             <span className="title"> Organika </span>
             <ul>
-                <li>Home</li>
-                <li>Catalog</li>
-                <li>About</li>
+                <li><Link to='/Home'> Home </Link> </li>
+                <li><Link to='/Catalog'> Catalog </Link></li>
+                <li><Link to='/Admin'> About </Link> </li>
+                <li><Link to='/Cart'> Cart </Link> </li>
             </ul>
         </div>
     );
